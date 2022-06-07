8aa0d4da063c32ac2abedc457492277274a85e83
Qasim Shaikh <87924988+shaikhmq20@users.noreply.github.com>
2022-06-07 09:17:44 +0530

    changing color for the status bar

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <HomeScreen />
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor='dodgerblue'/>
     </View>
   );
 }
